df5b00a87f49555b56c56d4c113f349c18c74de8
Jan Krakora <jan.krakora@kapsch.net>
2018-01-30 16:30:24 +0100

    Migration to Android Studio 3.0, About window info added

--- app/src/main/java/com/example/krakora/budgetkeeper/AboutActivity.java
+++ app/src/main/java/com/example/krakora/budgetkeeper/AboutActivity.java
@@ -6,6 +6,11 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewStub;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -16,7 +21,15 @@ public class AboutActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Fill the webview by About.html
+        WebView webview = (WebView) findViewById(R.id.about_webview);
+        WebSettings webSettings =
+              webview.getSettings();
+        webview.setWebChromeClient(
+                 new WebChromeClient());
+        webview.loadUrl("file:///android_asset/html/about.html");
 
+        // Home button
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
